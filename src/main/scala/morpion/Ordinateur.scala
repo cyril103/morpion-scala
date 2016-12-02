6f600bf20f37ccdad45cf55232f738a6b84586e1
cyril103 <cyril_10003@hotmail.fr>
2016-12-02 14:08:56 +0100

    modification sur trait Player

--- src/main/scala/morpion/Ordinateur.scala
+++ src/main/scala/morpion/Ordinateur.scala
@@ -4,23 +4,28 @@ package morpion
   * Created by cyril on 21/06/16.
   */
 case class Ordinateur(
-                       protected var tour:Boolean = false,
-                       val symbol:String = "O") extends Player {
 
+                       private var _tour: Boolean = false,
+                       symbol: String = "O") extends Player {
+
+  def tour = _tour
+
+  def tour_=(t: Boolean) = _tour = t
 
   def message = s"Ordinateur $symbol réféchis......."
 
-  def joue(tab :Array[Int]) = {
+  def joue(tab: Array[Int]) = {
 
-    tab(Evaluation2(tab,value)) = value
+    tab(Evaluation2(tab, value)) = value
 
-    tour = !tour
+    tour = !_tour
   }
 
 
 }
 
 object Ordinateur {
-  def X = new Ordinateur(symbol ="X")
-  def O = new Ordinateur(symbol ="O")
+  def X = new Ordinateur(symbol = "X")
+
+  def O = new Ordinateur(symbol = "O")
 }
